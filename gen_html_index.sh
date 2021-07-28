10e0e74fa06b44a6b612dcd31305fbc282192b19
Christian Nilsson <nikize@gmail.com>
2021-07-28 09:56:11 +0200

    Add README.md to html from gen_html_index.sh

--- gen_html_index.sh
+++ gen_html_index.sh
@@ -4,7 +4,7 @@ echo -e "<head><title>Gentoo minimal livecd over PXE, iPXE prefered</title>"
 echo -e "<meta charset=\"utf-8\"><meta name=viewport content=\"width=device-width, initial-scale=1\">"
 echo -e "</head><body>"
 echo 'See <a href="https://github.com/NiKiZe/Gentoo-iPXE">Gentoo iPXE on GitHub</a>'
-echo "<pre>"
+echo "<pre style=\"overflow:auto\">"
 thisscript=$(basename "$0")
 #FILES="gentoo gentoo.igz combined.igz image.squashfs *.iso $(git ls-files)"
 readarray -d '' files < <(printf '%s\0' gentoo gentoo.igz combined.igz image.squashfs *.iso $(git ls-files) | sort -zV)
@@ -21,3 +21,5 @@ else
 fi
 
 done
+echo "</pre>"
+markdown2 README.md || >&2 echo README.md conversion failed, emerge dev-python/markdown2 https://github.com/trentm/python-markdown2
