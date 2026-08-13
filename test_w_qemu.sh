e43040089231ca80ff7f6ed39a0cb78c4c529dda
Christian I. Nilsson <nikize@gmail.com>
2026-08-13 11:14:25 +0200

    Update test_w_qemu.sh to work with current qemu versions

--- test_w_qemu.sh
+++ test_w_qemu.sh
@@ -6,16 +6,17 @@ USEEFI=""
 VNC="-vnc 127.0.0.1:22"
 VGA=""
 efibios=""
-direct=("-boot" "menu=on")
+memorygb=2
 POSITIONAL=()
 while (($#)); do
   case $1 in
   useefi)
     USEEFI=YES
-    # >=sys-firmware/edk2-ovmf-202008
-    efibios="-bios /usr/share/edk2-ovmf/OVMF_CODE.fd"
-    # TODO fix proper chain
-    [ -f ipxe.efi ] || wget http://boot.ipxe.org/ipxe.efi
+    cp /usr/share/edk2-ovmf/OVMF_VARS.fd kvm_lxgentootest_VARS.fd
+    efibios="-drive if=pflash,unit=0,format=raw,readonly=on,file=/usr/share/edk2-ovmf/OVMF_CODE.fd \
+      -drive if=pflash,unit=1,format=raw,file=kvm_lxgentootest_VARS.fd"
+    cp $bootfile autoexec.ipxe
+    [ -f ipxe.efi ] || (wget https://boot.ipxe.org/x86_64-efi/ipxe-legacy.efi && mv ipxe-legacy.efi ipxe.efi)
     bootfile="ipxe.efi"
   ;;
   serial)
@@ -26,10 +27,17 @@ while (($#)); do
   useonline)
     # iPXE which is default in qemu, supports http boot
     bootfile="http://gentoo.ipxe.se/$bootfile"
+    # TODO add warning if online and serial since there will be no console
   ;;
   direct)
-    direct=("-kernel" "gentoo" "-initrd" "combined.igz" "-append" "root=/dev/ram0 init=/linuxrc  dokeymap looptype=squashfs loop=/image.squashfs  cdroot")
+    # TODO if serial add console
+    POSITIONAL+=("-kernel" "gentoo" "-initrd" "combined.igz" "-append" "dokeymap looptype=squashfs loop=/image.squashfs cdroot")
     bootfile=""
+  ;;
+  -m)
+    shift
+    echo "Set memory to $1 gb"
+    memorygb=$1
   ;;
   *)
     POSITIONAL+=("$1") # save it in an array for later
@@ -45,15 +53,22 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 [[ "$USEEFI" != "YES" ]] && [[ "$VGA" == "" ]] && VGA="-vga vmware"
 
 [[ "$VNC" != "" ]] && (sleep 3; vncviewer :22) &
+if [[ -z "$VNC" ]]; then
+  # if serial make sure autoexec.ipxe is set for serial console
+  [[ "$USEEFI" != "YES" ]] && cp $bootfile autoexec.ipxe && bootfile="autoexec.ipxe"
+  sed -i 's/ cdroot/ cdroot console=ttyS0,115200/' autoexec.ipxe
+  sed -i 's/vga=791//' autoexec.ipxe
+fi
 
 netscript="-nic user,model=virtio,tftp=.,bootfile=$bootfile"
 
 
 set -x
-jn=$(nproc)
-qemu-system-x86_64 -enable-kvm -M q35 -m 2048 -cpu host -smp $jn,cores=$jn,sockets=1 -name lxgentoopxetest \
+jn=$(($(nproc)/2))
+qemu-system-x86_64 -enable-kvm -M q35 -m $(($memorygb*1024)) -cpu host -smp $jn,cores=$jn,sockets=1 -name lxgentootest \
 $netscript \
--watchdog i6300esb -watchdog-action reset \
-"${direct[@]}" -usb ${VGA} ${VNC} \
+-device i6300esb -action watchdog=reset \
+-device virtio-rng-pci \
+-usb ${VGA} ${VNC} \
 ${efibios} \
-$POSITIONAL
+$*
