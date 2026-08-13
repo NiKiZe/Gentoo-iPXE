1a079078bc77d3418ef86d3357ca9e9f5bd49fdc
Christian I. Nilsson <nikize@gmail.com>
2026-08-14 00:00:51 +0200

    Fix gentoocd_unpack output and make cpio builds reproducible

--- gentoocd_unpack.sh
+++ gentoocd_unpack.sh
@@ -1,10 +1,45 @@
 #!/bin/bash
+# Some features from /lib/gentoo/functions.sh
+declare -- nl=$'\n'
+declare -- BRACKET=$'\E[34;01m'
+declare -- GOOD=$'\E[32;01m'
+declare -- WARN=$'\E[33;01m'
+declare -- BAD=$'\E[31;01m'
+declare -- NORMAL=$'\E[0m'
+declare -- BRBAD="${BRACKET}[ ${BAD}!!${BRACKET} ]${NORMAL}"
+declare -- BROK="${BRACKET}[ ${GOOD}ok${BRACKET} ]${NORMAL}"
+_indent=
+_eprint ()
+{
+    local color;
+    color=$1;
+    shift;
+    printf ' %s*%s %s%s' "${color}" "${NORMAL}" "${_indent}" "$*";
+}
+_ewnl ()
+{
+    test "${nl}" && ! case $1 in
+        *"${nl}")
+            false
+        ;;
+    esac
+}
+ebegin ()
+{
+    local msg;
+    msg=$*;
+    while _ewnl "${msg}"; do
+        msg=${msg%"${nl}"};
+    done;
+    _eprint "${GOOD}" "${msg} ...${nl}";
+}
+eerrorn () { _eprint "${BAD}" "$@" 1>&2; return 1; }
+eerror () { eerrorn "${*}${nl}"; }
+ewarnn () { _eprint "${WARN}" "$@" 1>&2; }
+ewarn () { ewarnn "${*}${nl}"; }
+einfon () { _eprint "${GOOD}" "$@"; }
+einfo () { einfon "${*}${nl}"; }
 set -euo pipefail
-ebegin() { echo $* ...; }
-eerror() { echo ERROR: $*; }
-einfo() { echo $*; }
-# Always verify script without this source after changes
-[[ -f /lib/gentoo/functions.sh ]] && source /lib/gentoo/functions.sh
 
 srciso=install-amd64-minimal-*.iso
 for f in $srciso; do
@@ -28,22 +63,30 @@ isoinfo -j UTF-8 -R -i ../${isoname} -X -find -path /image.squashfs && mv -vf im
 isoinfo -j UTF-8 -R -i ../${isoname} -X -find -path /boot/gentoo && mv -vf boot/gentoo ..
 isoinfo -j UTF-8 -R -i ../${isoname} -X -find -path /boot/gentoo.igz && mv -vf boot/gentoo.igz ..
 popd; rm -rf isoextract
-(cat gentoo.igz; (echo image.squashfs | cpio -H newc -o)) > combined.new.igz
-grubkernel=$(isoinfo -j UTF-8 -R -i ${isoname} -x /boot/grub/grub.cfg | grep "linux /boot" | grep -v docache)
-set +x
-[[ -z "$grubkernel" ]] && eerror "No kernel info from grub.cfg found"
-echo "... extraction done"
+
+read -r sqfs_size SOURCE_DATE_EPOCH < <(stat -c "%s %Y" image.squashfs)
+(cat gentoo.igz; cpio --reproducible -H newc -o <<< "image.squashfs" | pv -s $sqfs_size) > combined.new.igz
+# touch can be used to modify mtime of the combined file, but that is in a way lying so have opted not to
+#touch -d "@$SOURCE_DATE_EPOCH" combined.new.igz
+unset SOURCE_DATE_EPOCH
+#[ gentoo.igz -nt combined.new.igz ] && touch -r gentoo.igz combined.new.igz
 # only replace combined.igz if actually changed, to keep timestamps
 ([ ! -e combined.igz ] || !(cmp -s combined.new.igz combined.igz)) && mv -f combined.new.igz combined.igz
 [ -e combined.new.igz ] && rm -f combined.new.igz
 
+grubkernel=$(isoinfo -j UTF-8 -R -i ${isoname} -x /boot/grub/grub.cfg | grep "linux /boot" | grep -v \
+ -e docache)
+
+set +x
+echo " ... extraction done"
+[[ -z "$grubkernel" ]] && eerror "No kernel info from grub.cfg found"
 kernel=${grubkernel#*/boot/gentoo }
-einfo "Official kernel cmdline:\n     $kernel"
+einfo "Official kernel cmdline:$nl     $kernel"
 kernel=${kernel/dokeymap/\$\{keymap\}}
 for i in *.ipxe; do
   ipxekernel=$(grep "kernel gentoo " "$i" | sed "s/^.*kernel gentoo /gentoo /")
-  einfo "Checking for cmdline in $i:\n     $ipxekernel"
-  grep -q "$kernel" "$i" && echo " - Looks good" || echo " - Might need update"
+  einfo "Checking for cmdline in $i:$nl     $ipxekernel"
+  grep -q "$kernel" "$i" && echo " - Looks good $BROK" || echo " - Might need update $BRBAD"
 done
 
 # regenerate index
