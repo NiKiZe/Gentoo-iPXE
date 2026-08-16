38b2e074ada1b78fece19748911315485d5406ac
Christian I. Nilsson <nikize@gmail.com>
2026-08-16 21:48:14 +0200

    Add GH Action to run scheduled checks, test and create release

--- gentoocd_unpack.sh
+++ gentoocd_unpack.sh
@@ -123,6 +123,6 @@ for i in boot.ipxe; do
 done
 
 # regenerate index
-cp index.html index.bak.html
-sh gen_html_index.sh > index.html
-diff -u index.bak.html index.html
+cp index.html index.bak.html || true
+./gen_html_index.sh > index.html || true
+diff -u index.bak.html index.html || true
